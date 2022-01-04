aaa86fe7613aa0cb48861ce617a63e7fb592696e
natakkuma <95733427+natakkuma@users.noreply.github.com>
2022-01-03 21:47:44 -0800

    initial game setup 2

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,42 @@
+
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//    * Fight all enemy-robots
+//    * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
+
+//Player Variables
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+//Enemy Variables
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+// You can also log multiple values at once like this
+
+//Player Console Log
+console.log(playerName, playerAttack, playerHealth);
+
+//Enemy Console Log
+console.log(enemyNames.length);
+
+console.log(enemyNames[0]);
+console.log(enemyNames[1]);
+console.log(enemyNames[2]);
+
+//Enemy Console Loop
+for(var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index");
+  }
+
+
+//Fight Function
+var fight = function(enemyName) {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
     
@@ -65,4 +92,7 @@ var fight = function() {
     }
 }
 
-fight();
+//Fight Loop
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+  }
